478354bfdc5749945c8e4303553b475508bd20ab
sirsoco <66388516+sirsoco@users.noreply.github.com>
2020-06-22 14:10:53 -0500

    GET response added to .js(Back-end-team)

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,17 @@
+var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query + "&begin_date=" + yearStart + "&end_date=" + yearEnd + "&api-key=ZqwQqIWuyM9laRluApcFhucWcDEjlB6O"
 
-    // Example queryURL for Giphy API
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=d0zgc0GcMv2r08SDI5hJCS28BAP34oqJ
+
+    var query = $('#search-term').val();
+    var yearStart = $('#start-year').val();
+    var yearEnd = $('#end-year').val();
+             
+
     $.ajax({
-      url: queryURL,
+      url: queryUrl,
       method: "GET"
     }).then(function(response) {
       console.log(response);
 
+
       
-      }
-      
-    );
+      });
